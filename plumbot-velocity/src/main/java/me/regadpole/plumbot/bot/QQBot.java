7c9fa81c876cc38982d6345f256b80b543b725b5
风林Linye <168974367+LinyeFL@users.noreply.github.com>
2026-07-29 06:21:07 +0800

    feat: make QQ bot status notifications configurable

--- plumbot-velocity/src/main/java/me/regadpole/plumbot/bot/QQBot.java
+++ plumbot-velocity/src/main/java/me/regadpole/plumbot/bot/QQBot.java
@@ -37,133 +37,230 @@ public class QQBot implements Bot {
         qqEvent = new QQEvent(plugin);
         plugin.getLogger().info("QQ事件监听器注册完毕");
         plugin.getServer().getScheduler().buildTask(plugin, () -> {
-            http_config = new CQConfig(Config.bot.Bot.gocqhttp.HTTP, Config.bot.Bot.gocqhttp.Token, Config.bot.Bot.gocqhttp.IsAccessToken);
-            client =  new ClientFactory(http_config).createGroupClient();
-            messageClient =  new ClientFactory(http_config).createMessageClient();
-            LinkedBlockingQueue<String> blockingQueue = new LinkedBlockingQueue();//使用队列传输数据
+            http_config = new CQConfig(
+                    Config.bot.Bot.gocqhttp.HTTP,
+                    Config.bot.Bot.gocqhttp.Token,
+                    Config.bot.Bot.gocqhttp.IsAccessToken
+            );
+
+            client = new ClientFactory(http_config)
+                    .createGroupClient();
+
+            messageClient = new ClientFactory(http_config)
+                    .createMessageClient();
+
+            LinkedBlockingQueue<String> blockingQueue =
+                    new LinkedBlockingQueue<>();
+
             try {
-                connection = ConnectionFactory.createHttpServer(Config.bot.Bot.gocqhttp.ListenPort,"/",blockingQueue);
+                connection = ConnectionFactory.createHttpServer(
+                        Config.bot.Bot.gocqhttp.ListenPort,
+                        "/",
+                        blockingQueue
+                );
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
-            connection.create();
-            EventDispatchers dispatchers = new EventDispatchers(blockingQueue);//创建事件分发器
-            dispatchers.addListener(new SimpleListener<PrivateMessage>() {//私聊监听
-                @Override
-                public void onMessage(PrivateMessage privateMessage) {
-                    qqEvent.onFriendMessageReceive(privateMessage);
-                }
-            });
-            dispatchers.addListener(new SimpleListener<GroupMessage>() {//群聊消息监听
-                @Override
-                public void onMessage(GroupMessage groupMessage) {
-                    List<Long> groups = Config.bot.Groups;
-                    for (long groupID : groups) {
-                        if (groupID == groupMessage.getGroupId()) {
-                            qqEvent.onGroupMessageReceive(groupMessage);
-                        }
-                    }
-                }
-            });
-            dispatchers.addListener(new SimpleListener<GroupDecreaseNotice>() {//群聊人数减少监听
-                @Override
-                public void onMessage(GroupDecreaseNotice groupDecreaseNotice) {
-                    List<Long> groups = Config.bot.Groups;
-                    for (long groupID : groups) {
-                        if (groupID == groupDecreaseNotice.getGroupId()) {
-                            qqEvent.onGroupDecreaseNotice(groupDecreaseNotice);
-                        }
-                    }
-                }
-            });
-            dispatchers.start(10);//线程组处理任务
 
-            List<Long> groups = Config.bot.Groups;
-            for (long groupID : groups) {
-                sendGroupMsg("PlumBot已启动", groupID);
+            connection.create();
+
+            EventDispatchers dispatchers =
+                    new EventDispatchers(blockingQueue);
+
+            dispatchers.addListener(
+                    new SimpleListener<PrivateMessage>() {
+                        @Override
+                        public void onMessage(
+                                PrivateMessage privateMessage
+                        ) {
+                            qqEvent.onFriendMessageReceive(
+                                    privateMessage
+                            );
+                        }
+                    }
+            );
+
+            dispatchers.addListener(
+                    new SimpleListener<GroupMessage>() {
+                        @Override
+                        public void onMessage(
+                                GroupMessage groupMessage
+                        ) {
+                            List<Long> groups =
+                                    Config.bot.Groups;
+
+                            for (long groupID : groups) {
+                                if (groupID
+                                        == groupMessage
+                                        .getGroupId()) {
+
+                                    qqEvent.onGroupMessageReceive(
+                                            groupMessage
+                                    );
+                                }
+                            }
+                        }
+                    }
+            );
+
+            dispatchers.addListener(
+                    new SimpleListener<GroupDecreaseNotice>() {
+                        @Override
+                        public void onMessage(
+                                GroupDecreaseNotice
+                                        groupDecreaseNotice
+                        ) {
+                            List<Long> groups =
+                                    Config.bot.Groups;
+
+                            for (long groupID : groups) {
+                                if (groupID
+                                        == groupDecreaseNotice
+                                        .getGroupId()) {
+
+                                    qqEvent.onGroupDecreaseNotice(
+                                            groupDecreaseNotice
+                                    );
+                                }
+                            }
+                        }
+                    }
+            );
+
+            dispatchers.start(10);
+
+            if (Config.messages.Notifications
+                    .pluginStatusEnabled) {
+
+                List<Long> groups = Config.bot.Groups;
+
+                for (long groupID : groups) {
+                    sendGroupMsg(
+                            "PlumBot已启动",
+                            groupID
+                    );
+                }
             }
         }).schedule();
     }
 
     @Override
     public void shutdown() {
-        List<Long> groups = Config.bot.Groups;
-        for (long groupID : groups) {
-            sendMsg(true, "PlumBot已关闭", groupID);
+        if (Config.messages.Notifications
+                .pluginStatusEnabled) {
+
+            List<Long> groups = Config.bot.Groups;
+
+            for (long groupID : groups) {
+                sendMsg(
+                        true,
+                        "PlumBot已关闭",
+                        groupID
+                );
+            }
         }
+
         connection.stop();
     }
 
     @Override
-    public void sendMsg(boolean isGroup, String msg, long id) {
-        if (id == 0L) {return;}
-        if ("".equals(msg)) {return;}
-        plugin.getServer().getScheduler().buildTask(plugin, () -> {
-            if (isGroup) {
-                this.sendGroupMsg(msg, id);
-            } else {
-                this.sendPrivateMsg(msg, id);
-            }
-        }).schedule();
+    public void sendMsg(
+            boolean isGroup,
+            String msg,
+            long id
+    ) {
+        if (id == 0L) {
+            return;
+        }
+
+        if ("".equals(msg)) {
+            return;
+        }
+
+        plugin.getServer()
+                .getScheduler()
+                .buildTask(plugin, () -> {
+                    if (isGroup) {
+                        this.sendGroupMsg(msg, id);
+                    } else {
+                        this.sendPrivateMsg(msg, id);
+                    }
+                })
+                .schedule();
     }
 
-    public void sendCQMsg(boolean isGroup, String msg, long id) {
-        if (id == 0L) {return;}
-        if ("".equals(msg)) {return;}
-        plugin.getServer().getScheduler().buildTask(plugin, () -> {
-            if (isGroup) {
-                this.sendGroupCQMsg(msg, id);
-            } else {
-                this.sendPrivateCQMsg(msg, id);
-            }
-        }).schedule();
+    public void sendCQMsg(
+            boolean isGroup,
+            String msg,
+            long id
+    ) {
+        if (id == 0L) {
+            return;
+        }
+
+        if ("".equals(msg)) {
+            return;
+        }
+
+        plugin.getServer()
+                .getScheduler()
+                .buildTask(plugin, () -> {
+                    if (isGroup) {
+                        this.sendGroupCQMsg(msg, id);
+                    } else {
+                        this.sendPrivateCQMsg(msg, id);
+                    }
+                })
+                .schedule();
     }
 
     /**
      * 发送私聊消息
      */
-    public void sendPrivateMsg(String msg, long userID){
+    public void sendPrivateMsg(
+            String msg,
+            long userID
+    ) {
         messageClient.sendPrivateMsg(userID, msg);
     }
 
     /**
      * 发送群聊消息
      */
-    public void sendGroupMsg(String msg, long groupId) {
+    public void sendGroupMsg(
+            String msg,
+            long groupId
+    ) {
         messageClient.sendGroupMsg(groupId, msg);
     }
 
     /**
-     * 发送私聊消息
+     * 发送私聊 CQ 消息
      */
-    public void sendPrivateCQMsg(String msg, long userID){
-        messageClient.sendPrivateMsg(userID, msg, false);
+    public void sendPrivateCQMsg(
+            String msg,
+            long userID
+    ) {
+        messageClient.sendPrivateMsg(
+                userID,
+                msg,
+                false
+        );
     }
 
     /**
-     * 发送群聊消息
+     * 发送群聊 CQ 消息
      */
-    public void sendGroupCQMsg(String msg, long groupId) {
-        messageClient.sendGroupMsg(groupId, msg, false);
+    public void sendGroupCQMsg(
+            String msg,
+            long groupId
+    ) {
+        messageClient.sendGroupMsg(
+                groupId,
+                msg,
+                false
+        );
     }
-
-//    /**
-//     * 发送群聊消息 失败
-//     */
-//    public void sendGrouForwardMsg() {
-//        ForwardNode forwardNode = new ForwardNode();
-//        ForwardNode.Data data = forwardNode.getData();
-//        data.setName("slcower");
-//        data.setContent("消息");
-//        data.setSeq("消息");
-//        data.setId(1720939235);
-//        data.setUin(1452683658L);
-//        ArrayList<ForwardNode> nodes = new ArrayList();
-//        nodes.add(forwardNode);
-//        nodes.add(forwardNode);
-//        client.sendGroupForwardMsg(934145943L, nodes);
-//    }
-
 
     /**
      * 获取消息
@@ -177,7 +274,9 @@ public class QQBot implements Bot {
      * 获取转发消息
      */
     public void getForwardMsg(String msgId) {
-        List<ForwardMessage> msg = messageClient.getForwardMsg(msgId);
+        List<ForwardMessage> msg =
+                messageClient.getForwardMsg(msgId);
+
         System.out.println(msg);
     }
 
@@ -192,30 +291,44 @@ public class QQBot implements Bot {
     /**
      * 撤回消息
      */
-    public void deleteMsg(String msg, long groupId) {
-        messageClient.deleteMsg( messageClient.sendGroupMsg(groupId, msg));
+    public void deleteMsg(
+            String msg,
+            long groupId
+    ) {
+        messageClient.deleteMsg(
+                messageClient.sendGroupMsg(
+                        groupId,
+                        msg
+                )
+        );
     }
 
     /**
      * 获取群信息
      */
     public GroupInfo getGroupInfo(long groupId) {
-        GroupInfo groupInfo = client.getGroupInfo(groupId);
-        return groupInfo;
+        return client.getGroupInfo(groupId);
     }
 
     @Override
     public String getGroupName(long groupId) {
-        return getGroupInfo(groupId).getGroupName();
+        return getGroupInfo(groupId)
+                .getGroupName();
     }
 
     @Override
-    public boolean checkUserInGroup(long userId, long groupId) {
-        for (GroupMemberInfo member : client.getGroupMemberList(groupId)){
-            if(member.getUserId().equals(userId)){
+    public boolean checkUserInGroup(
+            long userId,
+            long groupId
+    ) {
+        for (GroupMemberInfo member
+                : client.getGroupMemberList(groupId)) {
+
+            if (member.getUserId().equals(userId)) {
                 return true;
             }
         }
+
         return false;
     }
 }
