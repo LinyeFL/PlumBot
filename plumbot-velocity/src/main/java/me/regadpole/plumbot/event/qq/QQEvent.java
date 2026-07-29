58ce6830ee93aa0ad61a7a38f9b4564c11626bde
风林Linye <168974367+LinyeFL@users.noreply.github.com>
2026-07-30 02:53:04 +0800

    Update QQEvent.java

--- plumbot-velocity/src/main/java/me/regadpole/plumbot/event/qq/QQEvent.java
+++ plumbot-velocity/src/main/java/me/regadpole/plumbot/event/qq/QQEvent.java
@@ -888,6 +888,8 @@ public class QQEvent {
         msg = msg.replaceAll("\\[CQ:xml,[^\\]]*\\]", "[XML]");
         // 其他未识别的 CQ 码，直接删除
         msg = msg.replaceAll("\\[CQ:[^\\]]*\\]", "");
+        // 清理新版 go-cqhttp 图片消息泄露的文件元数据
+        msg = msg.replaceAll("\\[图片\\],[^\\[]*(?=file_size=\\d+)[^\\[]*", "[图片]");     
         return msg;
     }
 
